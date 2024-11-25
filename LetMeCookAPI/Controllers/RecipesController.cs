ee41019e12cff5bf572852132f6ae71e5013e371
Jorge <jalquezar04@gmail.com>
2024-11-25 19:37:51 +0100

    test

--- LetMeCookAPI/Controllers/RecipesController.cs
+++ LetMeCookAPI/Controllers/RecipesController.cs
@@ -14,9 +14,9 @@ public class RecipesController : ControllerBase
     }
 
     [HttpGet]
-    public IActionResult GetPublicRecipes()
+    public IActionResult GetAll()
     {
-        var recipes = _service.GetPublicRecipes();
+        var recipes = _service.GetAll();
         return Ok(recipes);
     }
 
@@ -24,13 +24,48 @@ public class RecipesController : ControllerBase
     public IActionResult CreateRecipe([FromBody] Recipe recipe)
     {
         var createdRecipe = _service.CreateRecipe(recipe);
-        return CreatedAtAction(nameof(GetPublicRecipes), new { id = createdRecipe.Id }, createdRecipe);
+        return CreatedAtAction(nameof(GetAll), new { id = createdRecipe.Id }, createdRecipe);
     }
 
     [HttpPost("search")]
     public async Task<IActionResult> SearchRecipes([FromBody] RecipeSearchFilter filter)
     {
         var recipes = await _service.SearchRecipes(filter);
+        return Ok(recipes);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetRecipeById(int id)
+    {
+        var recipe = _service.GetRecipeById(id);
+        return Ok(recipe);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteRecipe(int id)
+    {
+        _service.DeleteRecipe(id);
+        return NoContent();
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateRecipe(int id, [FromBody] Recipe recipe)
+    {
+        var updatedRecipe = _service.UpdateRecipe(recipe);
+        return Ok(updatedRecipe);
+    }
+
+    [HttpGet("public")]
+    public IActionResult GetPublicRecipes()
+    {
+        var recipes = _service.GetPublicRecipes();
+        return Ok(recipes);
+    }
+
+    [HttpGet("user/{userId}")]
+    public IActionResult GetRecipesByUser(int userId)
+    {
+        var recipes = _service.GetRecipesByUser(userId);
         return Ok(recipes);
     }
 
