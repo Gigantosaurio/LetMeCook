8114e56e3b56d3ca0580bf8e11d71adea46afc9e
Jorge <jalquezar04@gmail.com>
2024-12-01 17:06:05 +0100

    Cambios en el builder de usuarios

--- LetMeCookAPI/Models/User.cs
+++ LetMeCookAPI/Models/User.cs
@@ -1,9 +1,30 @@
+using System.Security.Cryptography;
+
 namespace LetMeCookAPI.Models
 {
     public class User
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
         public string Username { get; set; }
         public string PasswordHash { get; set; }
+        private string PasswordSalt = "LetThemCook";
+        public DateTime Age { get; set; }
+
+        public User() { }
+
+        public User(int id, string name, string email, string username, string passwordHash, DateTime age)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Username = username;
+            MD5 md5Hash = MD5.Create();
+            byte[] passwordBytes = md5Hash.ComputeHash(System.Text.Encoding.UTF8.GetBytes(passwordHash + PasswordSalt));
+            PasswordHash = Convert.ToBase64String(passwordBytes);
+            Age = age;
+        }
+
     }
 }
